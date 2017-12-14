a92903c05efd85a713c9321a7f87a45f0323e10b
Jin-Sih Lin <linpct@gmail.com>
2017-12-15 02:30:02 +0800

    use coffeescript

--- app/App.js
+++ app/App.js
@@ -1,10 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+var instructions, styles;
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+
 import {
   Platform,
   StyleSheet,
@@ -12,12 +11,11 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu'
 });
+
 
 export default class App extends Component<{}> {
   render() {
@@ -36,22 +34,23 @@ export default class App extends Component<{}> {
     );
   }
 }
+;
 
-const styles = StyleSheet.create({
+styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {
   AppRegistry
 } from 'react-native';
 
-import App from './App';
+import App from './app/App';
 
 AppRegistry.registerComponent('appbase', () => {
   return App;
