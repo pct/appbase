c8ee050f8f216ba006ce24cda3f6aefd580efb10
Jin-Sih Lin <linpct@gmail.com>
2018-01-23 00:00:39 +0800

    add bundle to ios_deploy.sh

--- bin/ios_deploy.sh
+++ bin/ios_deploy.sh
@@ -2,5 +2,7 @@
 
 echo 'You must `npm i -g ios-deploy` first!'
 
+react-native bundle --entry-file index.js --platform ios --dev false --bundle-output ios/main.jsbundle --assets-dest ios
+
 #react-native run-ios --device "[DEVICE NAME]"
 react-native run-ios --device "pct_iphone"
