8393a31bfaf5a96e62f4b8f159576544c5e640e6
Jin-Sih Lin <linpct@gmail.com>
2018-01-23 00:08:51 +0800

    sync ios_deploy.sh

--- bin/ios_deploy.sh
+++ bin/ios_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+DEVICE_NAME='pct_iphone'
 
-echo 'You must `npm i -g ios-deploy` first!'
+echo 'You must `npm i -g ios-deploy` first to deploy ios app from command line'
 
 react-native bundle --entry-file index.js --platform ios --dev false --bundle-output ios/main.jsbundle --assets-dest ios
 
-#react-native run-ios --device "[DEVICE NAME]"
-react-native run-ios --device "pct_iphone"
+react-native run-ios --device $DEVICE_NAME --configuration Release
