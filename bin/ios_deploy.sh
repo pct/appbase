4abfd4da8122b775cb5e72c9d3bc352e492bb714
Jin-Sih Lin <linpct@gmail.com>
2018-02-21 16:21:18 +0800

    add bin/ios_deploy.sh

--- bin/ios_deploy.sh
+++ bin/ios_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 DEVICE_NAME='pct_iphone'
+PROJECT_NAME='appbase'
 
 echo 'You must `npm i -g ios-deploy` first to deploy ios app from command line'
 
 react-native bundle --entry-file index.js --platform ios --dev false --bundle-output ios/main.jsbundle --assets-dest ios
 
+cp ios/$PROJECT_NAME/AppDelegate.m.release ios/$PROJECT_NAME/AppDelegate.m
 react-native run-ios --device $DEVICE_NAME --configuration Release
